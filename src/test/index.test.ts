5dc94aef02d4fdc18c7c15fda48779d6312c84c2
Ashley Davis <ashley@codecapers.com.au>
2021-09-15 15:43:37 +1000

    Added a first basic test.

--- src/test/index.test.ts
+++ src/test/index.test.ts
@@ -1,10 +1,23 @@
+import { compile, genCode } from "..";
+import * as dedent from "dedent";
 
-describe("main test suite", () => {
+//
+// Normalize whitespace so we don't have to consider it when testing.
+//
+function normalize(input: string): string {
+    return input.split("\n").map(line => line.trim()).join("\n");
+}
 
-    it("should be true", ()  => {
+describe("aqua-compiler", () => {
 
-        expect(true).toBeTruthy();
+    it("can compile an expression", ()  => {
 
+        const teal = normalize(compile("1+1"));
+        const expectedTeal = normalize(dedent(`
+            int 1
+            int 1
+            +
+        `));
     });
 
 });
