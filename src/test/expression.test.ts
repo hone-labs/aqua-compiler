8b5acf16fa6e97e2d1bbf3c4f5879c348e5a5926
Ashley Davis <ashley@codecapers.com.au>
2021-09-21 16:42:07 +1000

    Refactor to separate out different types of tests.

--- src/test/expression.test.ts
+++ src/test/expression.test.ts
@@ -1,4 +1,4 @@
-import { compileExpression, genCode } from "..";
+import { compileExpression } from "..";
 import * as dedent from "dedent";
 
 //
@@ -8,7 +8,7 @@ function normalize(input: string): string {
     return input.split("\n").map(line => line.trim()).join("\n");
 }
 
-describe("aqua-compiler", () => {
+describe("expression", () => {
 
     //
     // Compile the input string and check it against the expected output.
@@ -111,21 +111,6 @@ describe("aqua-compiler", () => {
         );
     });
 
-    it("can generate code for operator", () => {
-
-        const opcode = "test-opcode";
-        const node = {
-            nodeType: "operator",
-            opcode: opcode,
-        };
-        const output: string[] = [];
-        genCode(node, output);
-        
-        expect(output).toEqual([
-            opcode,
-        ]);
-    });
-
     it("can get txn field", () => {
         check("txn Amount", "txn Amount");
     });
@@ -173,51 +158,5 @@ describe("aqua-compiler", () => {
             `)
         );
     });
-
-    it("can generate code for children", () => {
-
-        const opcode = "test-opcode";
-        const child1 = {
-            nodeType: "operator",
-            opcode: opcode + "-child-1",
-        };
-        const child2 = {
-            nodeType: "operator",
-            opcode: opcode + "-child-2",
-        };
-        const node = {
-            nodeType: "operator",
-            opcode: opcode + "-parent",
-            children: [
-                child1,
-                child2,
-            ],
-        };
-        const output: string[] = [];
-        genCode(node, output);
-        
-        expect(output).toEqual([
-            `${opcode}-child-1`,
-            `${opcode}-child-2`,
-            `${opcode}-parent`,
-        ]);
-    });
-
-    it("can generate code for literal", () => {
-
-        const opcode = "test-opcode";
-        const value = "1234";
-        const node = {
-            nodeType: "literal",
-            opcode: opcode,
-            value: value,
-        };
-        const output: string[] = [];
-        genCode(node, output);
-        
-        expect(output).toEqual([
-            `${opcode} ${value}`,
-        ]);
-    });    
 
 });
---
+++ src/test/gen-code.test.ts
@@ -0,0 +1,66 @@
+import { genCode } from "..";
+
+describe("code generator", () => {
+
+    it("can generate code for children", () => {
+
+        const opcode = "test-opcode";
+        const child1 = {
+            nodeType: "operator",
+            opcode: opcode + "-child-1",
+        };
+        const child2 = {
+            nodeType: "operator",
+            opcode: opcode + "-child-2",
+        };
+        const node = {
+            nodeType: "operator",
+            opcode: opcode + "-parent",
+            children: [
+                child1,
+                child2,
+            ],
+        };
+        const output: string[] = [];
+        genCode(node, output);
+        
+        expect(output).toEqual([
+            `${opcode}-child-1`,
+            `${opcode}-child-2`,
+            `${opcode}-parent`,
+        ]);
+    });
+
+    it("can generate code for operator", () => {
+
+        const opcode = "test-opcode";
+        const node = {
+            nodeType: "operator",
+            opcode: opcode,
+        };
+        const output: string[] = [];
+        genCode(node, output);
+        
+        expect(output).toEqual([
+            opcode,
+        ]);
+    });
+
+    it("can generate code for literal", () => {
+
+        const opcode = "test-opcode";
+        const value = "1234";
+        const node = {
+            nodeType: "literal",
+            opcode: opcode,
+            value: value,
+        };
+        const output: string[] = [];
+        genCode(node, output);
+        
+        expect(output).toEqual([
+            `${opcode} ${value}`,
+        ]);
+    });    
+
+});
