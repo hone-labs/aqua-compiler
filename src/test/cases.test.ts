31482bcf8f145abfaa40469db1e597075c27c247
Ashley Davis <ashley@codecapers.com.au>
2021-10-15 09:48:02 +1000

    Now including the test name in the output when the test fails, which makes it easier to associate output with the particular test.

--- src/test/cases.test.ts
+++ src/test/cases.test.ts
@@ -15,14 +15,15 @@ describe("test cases", () => {
     const testCaseFiles = glob.sync([`${__dirname.replace(/\\/g, "/")}/cases/**/input.aqua`]);
     for (const testCaseFile of testCaseFiles) {
         const basename = path.basename(path.dirname(testCaseFile));
-        it(`can compile ${basename}`, async () => {
+        const testName = `can compile ${basename}`;
+        it(testName, async () => {
             const dirPath = path.dirname(testCaseFile);
             const tealFilePath = path.join(dirPath, "output.teal");
             const input = await fs.promises.readFile(testCaseFile, "utf8");
             const compiled = normalize(compile(input));
             const expected = normalize(await fs.promises.readFile(tealFilePath, "utf8"));
             if (compiled !== expected) {
-                console.log(`Compiled:\r\n"${compiled}"\r\n\r\nExpected:\r\n"${expected}"`);
+                console.log(`== ${testName} ==\r\nCompiled:\r\n"${compiled}"\r\n\r\nExpected:\r\n"${expected}"`);
             }
             expect(compiled).toEqual(expected);    
         });
