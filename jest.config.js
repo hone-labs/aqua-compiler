2d268ae7d5dae5277ee71c9f8ce8cf7354fec6be
Ashley Davis <ashley@codecapers.com.au>
2021-10-14 15:30:05 +1000

    Removed a folder form Jest config that doesn't exist anymore.

--- jest.config.js
+++ jest.config.js
@@ -2,7 +2,6 @@ module.exports = {
     preset: 'ts-jest',
     testEnvironment: 'node',
     modulePathIgnorePatterns: [
-        "<rootDir>/build",
-        "<rootDir>/algo-builder",
+        "<rootDir>/build"
     ]
 };
