a32744af05fec374f82274b89cd3054fb2a28880
Ashley Davis <ashley@codecapers.com.au>
2021-10-21 11:37:24 +1000

    Added runtime based tests for while and for loops. These are currently commented out until we get an updated release of algo-builder/runtime.

--- src/test/runtime.test.ts
+++ src/test/runtime.test.ts
@@ -1,3 +1,4 @@
+import dedent from "dedent";
 import { compile } from "..";
 import { execute } from "../exec";
 
@@ -7,10 +8,35 @@ describe("runtime tests", () => {
         // Stub to prevent error about zero tests.
     })
 
-    // TODO: Enable this code once PR is accepted with algo-builder/runtime.
+    // // TODO: Enable this code once PR is accepted with algo-builder/runtime.
     // it("1 + 2", () => {
-    //     const result = execute(compile("1 + 2;"));
+    //     const result = execute(compile("return 1 + 2;"));
     //     expect(Number(result)).toBe(3);
     // });
 
+    // it("while loop", () => {
+
+    //     const result = execute(compile(dedent(`
+    //         let x = 0;
+    //         while (x < 2) {
+    //             x = x + 1;
+    //         }
+    //         return x;
+    //     `)));
+        
+    //     expect(Number(result)).toBe(2);
+    // });
+
+    // it("for loop", () => {
+
+    //     const result = execute(compile(dedent(`
+    //         let x = 0;
+    //         for (let i = 0; i < 4; i = i + 1) {
+    //             x = x + 1;
+    //         }
+    //         return x;
+    //     `)));
+        
+    //     expect(Number(result)).toBe(4);
+    // });
 });
