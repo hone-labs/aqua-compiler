226550d8dd0ff0e7c98765936127c1d53e383d55
Ashley Davis <ashley@codecapers.com.au>
2022-01-20 15:07:20 +1000

    Converted the parser to a class to make it easier to manage data and errors.

--- src/new_parser.ts
+++ src/new_parser.ts
@@ -5,81 +5,102 @@
 import { ASTNode } from "./ast";
 import { IToken, ITokenizer, Tokenizer, TokenType } from "./tokenizer";
 
-//
-// Parses an Aqua expression.
-//
-function expression(tokenizer: ITokenizer): ASTNode {
-    return term(tokenizer);
+export interface IParser {
+
+    //
+    // Parses an Aqua expression.
+    //
+    expression(tokenizer: ITokenizer): ASTNode;
+
 }
 
-//
-// Parses an addition/subtraction expression.
-//
-function term(tokenizer: ITokenizer): ASTNode {
-    let working = primary(tokenizer);
+export class Parser implements IParser {
+    
+    //
+    // Converts code into a stream of tokens.
+    //
+    private tokenizer: ITokenizer;
 
-    while (true) {
-        if (match(tokenizer, TokenType.PLUS)) {
-            const right = primary(tokenizer);
-            working = {
+    constructor(code: string) {
+        this.tokenizer = new Tokenizer(code);
+        this.tokenizer.readNext(); // Read first token.
+    }
+
+    //
+    // Parses an Aqua expression.
+    //
+    expression(): ASTNode {
+        return this.term();
+    }
+
+    //
+    // Parses an addition/subtraction expression.
+    //
+    private term(): ASTNode {
+        let working = this.primary();
+
+        while (true) {
+            if (this.match(TokenType.PLUS)) {
+                const right = this.primary();
+                working = {
+                    nodeType: "operation",
+                    opcode: "+",
+                    type: "integer",
+                    children: [
+                        working, 
+                        right,
+                    ],
+                };
+            }
+
+            //TODO: Add subtraction here.
+
+            break;
+        }
+
+        return working;
+    }
+
+    //
+    // Parse a primary compoent of an expression such as a number or identifier.
+    //
+    private primary(): ASTNode {
+        const numberToken = this.match(TokenType.NUMBER);
+        if (numberToken) {
+            return {
                 nodeType: "operation",
-                opcode: "+",
+                opcode: "int",
                 type: "integer",
-                children: [
-                    working, 
-                    right,
-                ],
-            };
+                args: [
+                    numberToken.value!,
+                ]
+            }
         }
 
-        //TODO: Add subtraction here.
-
-        break;
+        throw new Error(`Unexpected token`);
     }
 
-    return working;
-}
-
-//
-// Parse a primary compoent of an expression such as a number or identifier.
-//
-function primary(tokenizer: ITokenizer): ASTNode {
-    const numberToken = match(tokenizer, TokenType.NUMBER);
-    if (numberToken) {
-        return {
-            nodeType: "operation",
-            opcode: "int",
-            type: "integer",
-            args: [
-                numberToken.value!,
-            ]
+    //
+    // Matches the current token against an expected token. 
+    // If it matches the token is returned and the current token is advanced.
+    // If it doesn't match, undefined is returned.
+    //
+    private match(type: TokenType): IToken | undefined {
+        const curToken = this.tokenizer.getCurrent();
+        if (curToken && curToken.type === type) {
+            this.tokenizer.readNext();
+            return curToken;
+        }
+        else {
+            return undefined;
         }
     }
-
-    throw new Error(`Unexpected token`);
 }
 
 //
-// Matches the current token against an expected token. 
-// If it matches the token is returned and the current token is advanced.
-// If it doesn't match, undefined is returned.
-//
-function match(tokenizer: ITokenizer, type: TokenType): IToken | undefined {
-    const curToken = tokenizer.getCurrent();
-    if (curToken && curToken.type === type) {
-        tokenizer.readNext();
-        return curToken;
-    }
-    else {
-        return undefined;
-    }
-}
-
-//
-// Exported interface for testing.
+// Helper function for testing.
 //
 export function parseExpression(code: string): ASTNode {
-    const tokenizer = new Tokenizer(code);
-    tokenizer.readNext();
-    return expression(tokenizer);
+    const parser = new Parser(code);
+    return parser.expression();
 }
