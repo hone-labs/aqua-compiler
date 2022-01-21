119b79f483260098026fb081294965292e6cd7ef
Ashley Davis <ashley@codecapers.com.au>
2022-01-21 15:53:33 +1000

    Added tests to ensure the parser reports line and column for errors.

--- src/test/new_parser.test.ts
+++ src/test/new_parser.test.ts
@@ -1,6 +1,46 @@
 import { parse, parseExpression } from "../new_parser";
+import { IError } from "../tokenizer";
 
 describe("parser", () => {
+
+    //
+    // Parses code and only returns the errors produced.
+    //
+    function retreiveErrors(code: string): IError[] {
+
+        let errors: IError[] = [];
+
+        parse(code, err => {
+            errors.push(err);
+        });        
+
+        return errors;
+    }
+
+    //
+    // Expect a single object to contain expected fields.
+    //
+    function expectFields(actual: any, expected: any): void {
+        for (const [key, value] of Object.entries(expected)) {
+            const actualValue = actual[key];
+            const expectedValue  = expected[key];
+            if (actualValue !== expectedValue) {
+                throw new Error(`Expected "${key}" to be set to ${value}\r\nActual value: ${actualValue}\r\nExpected value: ${expectedValue}\r\nActual object: ${JSON.stringify(actual, null, 4)}`);
+            }            
+        }
+    }
+
+    //
+    // Expect an array of object to contain expected fields.
+    //
+    function expectArray(actual: any[], expected: any[]): void {
+        
+        expect(actual.length).toEqual(expected.length);
+
+        for (let i = 0; i < actual.length; ++i) {
+            expectFields(actual[i], expected[i]);
+        }
+    }    
 
     test("can parse number expression", () => {
 
@@ -89,5 +129,13 @@ describe("parser", () => {
             errorReported = true;
         });
         expect(errorReported).toEqual(true);
+    });
+
+    test("error reports line and column", () => {
+
+        expectArray(retreiveErrors("@;"), [{ line: 1, column: 0 }]);
+        expectArray(retreiveErrors("\n@;"), [{ line: 2, column: 0 }]);
+        expectArray(retreiveErrors(" @;"), [{ line: 1, column: 1 }]);
+        expectArray(retreiveErrors("\n @;"), [{ line: 2, column: 1 }]);
     });
 });
