eac52a8c397566f033a19a0f15f2e6edac083ade
Ashley Davis <ashley@codecapers.com.au>
2022-07-15 13:47:33 +1000

    Change the Webpack require context to use .ts file extension. This was required to be able to read the visitor plugins after the library itself is bundled with Webpack.

--- src/code-generator/index.ts
+++ src/code-generator/index.ts
@@ -11,7 +11,7 @@ export const visitors: INodeVisitorMap = {};
 function loadVisitors() {
     if (require.context) {
         // https://webpack.js.org/configuration/module/#module-contexts
-        const visitorsContext = require.context("./visitors", true, /\.\/.*\.js$/);
+        const visitorsContext = require.context("./visitors", true, /\.\/.*\.ts$/);
         for (const key of visitorsContext.keys()) {
             const nodeName = key.substring(2, key.length - 3);
             visitors[nodeName] = visitorsContext(key).default;
